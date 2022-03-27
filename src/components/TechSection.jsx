830bcf812f8627b0ef6b0c4bb0402f2e7eeb3f53
Santiago <schuckmannsantiago@gmail.com>
2022-03-27 17:55:19 -0300

    creating tech section

---
+++ src/components/TechSection.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const TechSection = () => {
+  return (
+    <>
+    <section className='me'>
+      <h3>Do I know anything?</h3>
+      <p> Actually, I would say otherwise. My greatest virtue is that i am not able to grasp on the idea of being "totally" skillful at something. Luckily, this haven´t stopped me from building and working on projects, despite knowing I can always learn something new. Being totally honest...</p> 
+      <h4>I can deliver in this skills </h4> 
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript(ES6)</li>
+        <li>React.js</li>
+      </ul>
+      <h4>I am learning these skills. Hope seeing these up there 😋</h4>
+      <ul>
+        <li>Next.js</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+      </ul>
+      <span>Is not so difficult to see that what I want, what I need is a... to become a Full Stack Developer in JavaScript.</span>
+    </section>
+    <style jsx>
+      {`
+      .me {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+        justify-content: center
+      }
+      h4 {
+        border: 1px solid var(--secondary-color)
+      }
+      span {
+        font-size: 2rem;
+        text-align: left;
+      }`}
+    </style>
+    </>
+  )
+}
+
+export default TechSection;
